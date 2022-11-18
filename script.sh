7b2bfe5ede4c35df4255a8bef19d58bf194a993c
Luka Stojic <luka@arthaus.info>
2022-11-18 10:54:55 +0100

    initial commit, working script

---
+++ script.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+# ffmpeg -i Postetikette-Drucken.mp4 -c:v libx264 -b:v 8M -crf 24 ./Postetiketten-drucken.mp4
+# script changes bitrate to 8Mbps and 24 frames to make files smaller
+# it takes an input folder as first and output folder as second argument
+
+if [ $# -ne 2 ]
+then
+	echo "Example usage: ./script.sh inputFolder outputFolder"
+	exit 1
+fi
+
+if [[ ! -d $1 ]]
+then
+	echo "Input directory does not exist!"
+	exit 1
+fi
+
+if [[ ! -d $2 ]]
+then
+	echo "Output directory does not exist!"
+	exit 1
+fi
+
+
+for file in $1/*.mp4
+do
+	if [[ -f $file ]]; then
+		echo "Compressing File: $file"
+		filename="$(basename $file)"
+		ffmpeg -i $file -c:v libx264 -b:v 8M -crf 24 "$2/$filename"
+	fi
+done
